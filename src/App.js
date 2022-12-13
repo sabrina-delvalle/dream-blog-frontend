3f850ff336f68ce2cd8c0a6c3e4881e567e38479
Sabrina <is.sabrinadelvalle@gmail.com>
2022-12-13 00:05:13 -0400

    test links

--- src/App.js
+++ src/App.js
@@ -26,16 +26,16 @@ function App() {
         <UserContext.Provider value={providerValue}>
           <Routes>
             <Route path='/' element={<Index/>} />
-            <Route path='/login' element={<Login/>} />
-            <Route path='/register' element={<Register/>} />
-            <Route path='/auth' element={<Auth/>} />
-            <Route path='/user/:id' element={<User />} />
-            <Route path='/post/:id' element={<PostID />} />
-            <Route path='/done' element={<Done />} />
+            <Route path='https://dream-bg-api.onrender.com/login' element={<Login/>} />
+            <Route path='https://dream-bg-api.onrender.com/register' element={<Register/>} />
+            <Route path='https://dream-bg-api.onrender.com/auth' element={<Auth/>} />
+            <Route path='https://dream-bg-api.onrender.com/user/:id' element={<User />} />
+            <Route path='https://dream-bg-api.onrender.com/post/:id' element={<PostID />} />
+            <Route path='https://dream-bg-api.onrender.com/done' element={<Done />} />
 
             <Route element={<ProtectedRoutes/>}>
-              <Route path='/new-post' element={<PostPage/>} />  
-              <Route path='/profile' element={<Profile/>} /> 
+              <Route path='https://dream-bg-api.onrender.com/new-post' element={<PostPage/>} />  
+              <Route path='https://dream-bg-api.onrender.com/profile' element={<Profile/>} /> 
             </Route>
           
             <Route path='*' element={<NotFound/>} />
