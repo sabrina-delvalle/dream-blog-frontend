38b09d4b6d273dba873a8c4a70bd6f577985e492
Sabrina <is.sabrinadelvalle@gmail.com>
2022-12-13 13:48:04 -0400

    redirects 2

--- src/App.js
+++ src/App.js
@@ -26,16 +26,16 @@ function App() {
         <UserContext.Provider value={providerValue}>
           <Routes>
             <Route path='/' element={<Index/>} />
-            <Route path='login' element={<Login/>} />
-            <Route path='register' element={<Register/>} />
-            <Route path='auth' element={<Auth/>} />
-            <Route path='user/:id' element={<User />} />
-            <Route path='post/:id' element={<PostID />} />
-            <Route path='done' element={<Done />} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/auth' element={<Auth/>} />
+            <Route path='/user/:id' element={<User />} />
+            <Route path='/post/:id' element={<PostID />} />
+            <Route path='/done' element={<Done />} />
 
             <Route element={<ProtectedRoutes/>}>
-              <Route path='new-post' element={<PostPage/>} />  
-              <Route path='profile' element={<Profile/>} /> 
+              <Route path='/new-post' element={<PostPage/>} />  
+              <Route path='/profile' element={<Profile/>} /> 
             </Route>
           
             <Route path='*' element={<NotFound/>} />
